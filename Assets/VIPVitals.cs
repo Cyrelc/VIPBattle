c1711a9ac4ed67a66c9be9951ae0671c18a0c2aa
cmassie <massie@Christophers-MacBook-Air.local>
2016-04-17 19:27:08 -0600

    VIPs can die and soldiers can hit them

---
+++ Assets/VIPVitals.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VIPVitals : MonoBehaviour {
+
+	//Handles VIP health and existence in game
+
+	public int hp; 
+	private Create_Teams CT;
+	// Use this for initialization
+	void Start () {
+		hp = 30; // Hp of VIPs 
+		CT = GameObject.Find ("controller").GetComponent<Create_Teams> (); // the accessor for the list we need to remove VIPs from when they are destroyed
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hp <= 0) {
+			// if this VIP is dead
+			CT.VIPs.Remove (transform); // remove from list of VIPs for win condition
+			Destroy (transform.gameObject); // destroy this object
+		}
+	}
+}
